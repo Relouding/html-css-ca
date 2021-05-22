d4b8ec44c6028317d3af2fee90105e2b4db263d7
Andreas <74594568+Relouding@users.noreply.github.com>
2021-05-22 10:35:14 +0200

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,11 @@
-js
+<!-- Hotjar Tracking Code for Relouding.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2414853,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
